f8d9ce00f1f06fcfa6cb8758ed026f79317679bc
mathh1337 <112662623+matipt2@users.noreply.github.com>
2023-10-11 14:16:18 +0200

    check

--- t1/src/Main.java
+++ t1/src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
-        Pesel pesel1 = new Pesel(args[0]);
-        Pesel pesel2 = new Pesel(args[1]);
-        System.out.println("Pesel 1:"+args[0]);
-        System.out.println("Pesel 2:"+args[1]);
-
-        Pesel.check(pesel1);
-        Pesel.check(pesel2);
-        Pesel.compare(pesel1, pesel2);
-
+        if (args.length == 1) {
+            Pesel pesel1 = new Pesel(args[0]);
+            pesel1.check();
+        } else if (args.length == 2) {
+            Pesel pesel1 = new Pesel(args[0]);
+            Pesel pesel2 = new Pesel(args[1]);
+            pesel1.compare(pesel2);
+        } else {
+            System.out.println("Nieprawidłowe argumenty");
+        }
     }
 }
--- t1/src/Pesel.java
+++ t1/src/Pesel.java
@@ -1,43 +1,24 @@
 public class Pesel {
     private String pesel;
-    private static int length;
 
-
-    public String getPesel() {
-        return pesel;
-    }
-
-    public void setPesel(String pesel) {
+    public Pesel(String pesel) {
         this.pesel = pesel;
     }
 
-    public static int getLength() {
-        return length;
-    }
-
-
-    public Pesel(String pesel){
-
-        this.pesel = pesel;
-    }
-
-    public static void compare(Pesel pesel1, Pesel pesel2){
-        boolean areEqual = pesel1.equals(pesel2);
-        if(areEqual == true){
+    public void compare(Pesel anotherPesel) {
+        boolean areEqual = this.pesel.equals(anotherPesel.pesel);
+        if (areEqual) {
             System.out.println("Identyczne");
-        }
-        else{
+        } else {
             System.out.println("Inne");
         }
     }
 
-    public static void check(Pesel pesel){
-        int length = pesel.length;
-        if(length == 9){
-            System.out.println("jest git");
-        }
-        else{
-            System.out.println("nie jest git");
+    public void check() {
+        if (pesel.length() == 11) {
+            System.out.println("Pesel jest poprawny");
+        } else {
+            System.out.println("Pesel jest niepoprawny");
         }
     }
 }
