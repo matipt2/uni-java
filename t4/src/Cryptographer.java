7eab99825623a6a04b84f3003ef8d8a25fceac91
mathh1337 <112662623+matipt2@users.noreply.github.com>
2023-10-30 14:59:53 +0100

    commit

--- t4/src/Cryptographer.java
+++ t4/src/Cryptographer.java
@@ -1,8 +1,33 @@
-public class Cryptographer {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-    public static void cryptfile(String path_to_file_in, String path_to_file_out, Algorithm algorithm){
-        
+public class Cryptographer {
+    public static void cryptfile(String path_to_file_in, String path_to_file_out, Algorithm algorithm) {
+        BufferedReader reader;
+        FileWriter myWriter;
+
+        try {
+            reader = new BufferedReader(new FileReader(path_to_file_in));
+            myWriter = new FileWriter(path_to_file_out);
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                String encryptedLine = algorithm.crypt(line);
+                myWriter.write(encryptedLine + "\n");
+                line = reader.readLine();
+            }
+
+            reader.close();
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
     public static void decryptfile(String path_to_file_in, String path_to_file_out, Algorithm algorithm){
 
     }
--- t4/src/Main.java
+++ t4/src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        String path_to_file_in = "/Users/mateuszptasik/Desktop/poli-input.txt";
+        String path_to_file_out = "/Users/mateuszptasik/Desktop/text.txt";
         Polibiusz poli = new Polibiusz();
         ROT11 rot11 = new ROT11();
-
-
-        System.out.println(poli.crypt("LOL"));
-        System.out.println(poli.decrypt("15 22 15"));
-        System.out.println(rot11.crypt("lol"));
-        System.out.println(rot11.decrypt("wzw"));
+        Cryptographer.cryptfile(path_to_file_in, path_to_file_out, poli);
     }
 }
