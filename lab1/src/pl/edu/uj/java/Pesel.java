a66ee524f2ddb11abd8d93f62d2ad8308a50d7e3
matipt2 <mateuszpt2@o2.pl>
2023-11-21 16:27:46 +0100

    commit

--- lab1/src/pl/edu/uj/java/Pesel.java
+++ lab1/src/pl/edu/uj/java/Pesel.java
@@ -1,10 +1,9 @@
-package pl.edu.uj.java;
 public class Pesel {
     private String pesel;
 
     public String getPesel() {
         return pesel;
-    } //getter
+    }
 
     public void setPesel(String pesel) {
         this.pesel = pesel;
@@ -15,36 +14,29 @@ public class Pesel {
     }
 
     public boolean compare(Pesel anotherPesel) {
-        if (!check(this) || !check(anotherPesel)) { // sprawdzenie, czy sa prawidlowe przed porownaniem
-            return false;
-        }
         boolean areEqual = this.pesel.equals(anotherPesel.pesel);
         return areEqual;
     }
 
-    public static boolean check(Pesel pesel) {
-        String peselStr = pesel.getPesel();
-        if (peselStr.length() != 11) {
+    public static boolean check(String pesel) {
+        if (pesel.length() != 11) {
             return false;
         }
 
         int sum = 0;
-        int[] waga = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3}; //wagi z wikipedii
+        int[] waga = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
 
         for (int i = 0; i < 10; i++) {
-            int index = Character.getNumericValue(peselStr.charAt(i)); //getNumericValue, zeby zamienic Stringi na cyfry
-            if(index<0 || index>9){ //sprawdzenie czy w Peselu znajduja sie tylko cyfry
-                return false;
-            }
+            int index = Character.getNumericValue(pesel.charAt(i)); 
             sum += waga[i] * index;
         }
 
         int kontrolna = 10 - (sum % 10);
-        if (kontrolna == 10) { // w razie przypadku szczegolnego z wikipedii
+        if (kontrolna == 10) { 
             kontrolna = 0;
         }
 
-        int cyfraKontrolna = Character.getNumericValue(peselStr.charAt(10));
+        int cyfraKontrolna = Character.getNumericValue(pesel.charAt(10));
         return kontrolna == cyfraKontrolna;
     }
 
