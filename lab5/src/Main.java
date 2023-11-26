9e01954a61a205b08183a1728c7f90ce4848b4b7
matipt2 <mateuszpt2@o2.pl>
2023-11-26 22:10:24 +0100

    commit

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -1,5 +1,5 @@
 import java.io.File;
-
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String path_in = args[0];
@@ -8,17 +8,48 @@ public class Main {
         int fps = Integer.parseInt(args[3]);
 
         try {
-            File newFile = new File(path_in);
-            if (newFile.length() == 0) {
-                throw new MicroException("file is empty");
-            } else {
-                MicroDVDDelay.delay(path_in, path_out, delay, fps);
-            }
-
+            MicroDVDDelay.delay(path_in, path_out, delay, fps);
+        } catch (FrameOrderException e) {
+            System.err.println("FrameOrderException: Error at line " + e.getErrorLine() + ": " + e.getMessage());
+        } catch (FrameNumberException e) {
+            System.err.println("FrameNumberException: Error at line " + e.getErrorLine() + ": " + e.getMessage());
+        } catch (FrameException e) {
+            System.err.println("FrameException: Error at line " + e.getErrorLine() + ": " + e.getMessage());
         } catch (MicroException e) {
-            System.err.println("Error at line " + e.getErrorLine() + ": " + e.getMessage());
+            System.err.println("MicroException: Error at line " + e.getErrorLine() + ": " + e.getMessage());
         } catch (Exception e) {
             System.err.println("Unknown error: " + e.getMessage());
         }
+    }
+}
+
+class MicroException extends Exception {
+    private final int errorLine;
+
+    public MicroException(String message, int errorLine) {
+        super(message);
+        this.errorLine = errorLine;
+    }
+
+    public int getErrorLine() {
+        return errorLine;
+    }
+}
+
+class FrameException extends MicroException {
+    public FrameException(String message, int errorLine) {
+        super(message, errorLine);
+    }
+}
+
+class FrameOrderException extends FrameException {
+    public FrameOrderException(String message, int errorLine) {
+        super(message, errorLine);
+    }
+}
+
+class FrameNumberException extends FrameException {
+    public FrameNumberException(String message, int errorLine) {
+        super(message, errorLine);
     }
 }
--- lab5/src/MicroDVDDelay.java
+++ lab5/src/MicroDVDDelay.java
@@ -1,54 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class MicroDVDDelay {
-    public static void delay(String in, String out, int delay, int fps) throws MicroException {
-        try {
-            List<String> lines = Files.readAllLines(Paths.get(in));
-            Pattern pattern = Pattern.compile("\\{(\\d+)}");
+    public static void delay(String in, String out, int delay, int fps) throws MicroException, IOException {
+        Path inputFilePath = Paths.get(in);
+        Path outputFilePath = Paths.get(out);
 
-            for (int i = 0; i < lines.size() - 1; i++) {
-                String line = lines.get(i);
-                String nextLine = lines.get(i + 1);
+        int lineNumber = 0;
+        try (BufferedReader reader = Files.newBufferedReader(inputFilePath, StandardCharsets.UTF_8);
+             BufferedWriter writer = Files.newBufferedWriter(outputFilePath, StandardCharsets.UTF_8)) {
+
+            Pattern pattern = Pattern.compile("\\{(\\d+)}");
+            String line;
+            lineNumber = 0;
+
+            while ((line = reader.readLine()) != null) {
+                lineNumber++;
+
                 Matcher matcher = pattern.matcher(line);
-                Matcher nextMatcher = pattern.matcher(nextLine);
                 StringBuffer stringBuffer = new StringBuffer();
 
-                while (matcher.find() && nextMatcher.find()) {
+                while (matcher.find()) {
                     int currentNumber = Integer.parseInt(matcher.group(1));
-                    int nextNumber = Integer.parseInt(nextMatcher.group(1));
-                    if (currentNumber >= nextNumber) {
-                        throw new MicroException("invalid sequence at line " + (i + 1), i + 1);
-                    }
                     currentNumber += (delay * fps) / 1000;
                     matcher.appendReplacement(stringBuffer, "{" + currentNumber + "}");
                 }
 
                 matcher.appendTail(stringBuffer);
-                lines.set(i, stringBuffer.toString());
+                writer.write(stringBuffer.toString());
+                writer.newLine();
+
+                validateSequence(line, lineNumber);
             }
-
-            String lastLine = lines.get(lines.size() - 1);
-            Matcher lastMatcher = pattern.matcher(lastLine);
-            StringBuffer lastStringBuffer = new StringBuffer();
-
-            while (lastMatcher.find()) {
-                int currentNumber = Integer.parseInt(lastMatcher.group(1));
-                currentNumber += (delay * fps) / 1000;
-                lastMatcher.appendReplacement(lastStringBuffer, "{" + currentNumber + "}");
-            }
-
-            lastMatcher.appendTail(lastStringBuffer);
-            lines.set(lines.size() - 1, lastStringBuffer.toString());
-
-            Files.write(Paths.get(out), lines);
 
         } catch (IOException e) {
-            throw new RuntimeException("Something went wrong", e);
+            throw new IOException("Error reading or writing files", e);
+        }
+    }
+
+    private static void validateSequence(String line, int lineNumber) throws FrameException {
+        Pattern sequencePattern = Pattern.compile("\\{(\\d+)}(\\{(\\d+)})?");
+        Matcher sequenceMatcher = sequencePattern.matcher(line);
+
+        while (sequenceMatcher.find()) {
+            int startFrame = Integer.parseInt(sequenceMatcher.group(1));
+            int endFrame = sequenceMatcher.group(3) != null ? Integer.parseInt(sequenceMatcher.group(3)) : startFrame;
+
+            if (startFrame > endFrame) {
+                throw new FrameOrderException("Start frame is greater than end frame", lineNumber);
+            }
+
+            if (!sequenceMatcher.group(1).matches("\\d+") ||
+                    (sequenceMatcher.group(3) != null && !sequenceMatcher.group(3).matches("\\d+"))) {
+                throw new FrameNumberException("Invalid frame number format", lineNumber);
+            }
         }
     }
 }
