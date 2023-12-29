40df3bed2692aa75d68fbd554b7bda1eb4e9657d
matipt2 <mateuszpt2@o2.pl>
2023-12-29 16:18:32 +0100

    Update URLConnectionExample.java

--- Network/src/URLConnectionExample.java
+++ Network/src/URLConnectionExample.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class URLConnectionExample {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://www.google.pl/");
+        URLConnection con = url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String line;
+        while((line=in.readLine())!=null){
+            System.out.println(line);
+        }
+        in.close();
     }
 }
