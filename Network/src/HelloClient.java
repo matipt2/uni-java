b87a687d7df23ddbe6b41dd342ee494b8f8c6565
matipt2 <mateuszpt2@o2.pl>
2023-12-31 14:34:45 +0100

    echo

--- Network/src/HelloClient.java
+++ Network/src/HelloClient.java
@@ -1,13 +1,11 @@
-import javax.xml.crypto.Data;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class HelloClient {
     public static void main(String[] args) throws Exception{
         Socket s = new Socket("localhost", 5000);
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
         String line = br.readLine();
