24d19caee006cce8d3adc158b70fee9e42d777ba
mathh1337 <112662623+matipt2@users.noreply.github.com>
2024-01-03 02:27:55 +0100

    collections

---
+++ Collections/src/HashMapExample.java
@@ -0,0 +1,5 @@
+public class HashMapExample {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Collections/src/HashSetExample.java
@@ -0,0 +1,6 @@
+public class HashSetExample
+{
+    public static void main(String[] args) {
+        
+    }
+}
