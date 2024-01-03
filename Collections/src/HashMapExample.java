ba9d2c0fc632f9d1b8607cce19ba5b91112b0bcb
mathh1337 <112662623+matipt2@users.noreply.github.com>
2024-01-03 02:56:34 +0100

    hashset/hashmap

--- Collections/src/HashMapExample.java
+++ Collections/src/HashMapExample.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapExample {
     public static void main(String[] args) {
 
+        Map<String,Integer> hashMap = new HashMap<>();
+        hashMap.put("Mateusz",123);
+        hashMap.put("Kacper", 321);
+        hashMap.put("Filip",456);
+        System.out.println(hashMap.get("Filip"));
+
+        for(Map.Entry<String,Integer> entry: hashMap.entrySet()){
+            String key = entry.getKey();
+            int value = entry.getValue();
+            System.out.println("Key: "+key+" Value: "+value);
+        }
     }
 }
