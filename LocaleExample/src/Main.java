ca5fb2f90d1671768a2a1afa807137d5f359a048
mathh1337 <112662623+matipt2@users.noreply.github.com>
2024-01-11 01:35:38 +0100

    locale/cloneable

--- LocaleExample/src/Main.java
+++ LocaleExample/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale locale = new Locale("pl", "PL");
+        Locale.setDefault(locale);
+//        ResourceBundle message = ResourceBundle.getBundle("messages", locale);
+//        System.out.println(message);
+        String lang = locale.getLanguage();
+        String country = locale.getCountry();
+        System.out.println(lang+country);
+        }
     }
-}
